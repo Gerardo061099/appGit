df39992b45db93166bfe4dc2f574cf6cb195a987
Gerardo061099 <87624313+Gerardo061099@users.noreply.github.com>
2025-07-11 16:35:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ console.log("Git Course!!");
 console.log("This is a new line added to the file.");
 
 console.log("Rama loggin creada");
+
+asdsdasd
+asdasdsa
+dsdasdas
+asdasd
+asdasdsa
+sadasdsa
+sad
+console.error("Error en el código")

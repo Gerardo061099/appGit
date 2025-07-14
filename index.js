d45b5149a03062fd6ebb0f7fddb8f4b9a50f42d6
Gerardo J.C <ger4rdojimenezc4zs@gmail.com>
2025-07-14 15:20:07 -0600

    change one

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Funcion main optimizada");
 
-function main(a,b,c,d){
-  const ope = a + b;
-  console.log(ope*c*d)
+function main(a, b, c, d) {
+  const ope = a / b;
+  console.log(ope);
 }
